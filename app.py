602e8bb2d42972eabe656832cbbfb3af3a591c9d
Krzysztof Krysiak <krysiak86@gmail.com>
2020-01-15 13:48:47 +0100

    Add update endpoint

--- app.py
+++ app.py
@@ -43,7 +43,18 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    return '';
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task=task)
 
 
 @app.route('/blabla')
