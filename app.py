45a7e5035cabe82ab57b81dad508265668737c18
Krzysztof Krysiak <krysiak86@gmail.com>
2020-01-15 11:56:37 +0100

    Add database and .vscode

--- app.py
+++ app.py
@@ -1,39 +1,62 @@
-﻿from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}'
 
-@app.route('/world')
+
+@app.route('/world/')
 def world():
     return render_template('index.html')
+
 
 @app.route('/api/')
 def api_version():
     return 'Api page'
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % escape(username)
 
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
 
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     # show the subpath after /path/
     return 'Subpath %s' % escape(subpath)
 
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
 
+
 @app.route('/about')
 def about():
     return 'The about page'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
